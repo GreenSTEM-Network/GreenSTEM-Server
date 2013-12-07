67c61df2404e27b509b6182bfd3db22e93f57339
Jason Blanchard <jas.blanchard@gmail.com>
2013-12-07 10:41:44 -0500

    Working on the views

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,8 +1,8 @@
 class SitesController < ApplicationController
     def show
         @site = Site.find(params[:id])
-        @nodes = @site.nodes
-        @data = @nodes.node_readings
+        @nodes = @site.nodes.includes(:readings)
+        @readings = @nodes.map(&:readings).flatten
 
         respond_to do |format|
             format.html

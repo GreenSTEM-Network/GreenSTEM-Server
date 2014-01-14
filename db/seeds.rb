c7b623aa5e34ec01ca30f19063e12493fdf1919a
Kevin Clough <kevinclough@gmail.com>
2014-01-14 18:57:38 -0500

    fix db seeds to work without environment variable

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) are set in the file config/application.yml.
 # See http://railsapps.github.com/rails-environment-variables.html
-puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
-  puts 'role: ' << role
-end
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
-user.add_role :admin
 
+adminRole = Role.create! :name => 'admin'
+userRole = Role.create! :name => 'user'
+
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :email => 'user@example.com', :password => 'password1', :password_confirmation => 'password1', :name => 'John Doe'
+user2 = User.create! :email => 'user2@example.com', :password => 'password1', :password_confirmation => 'password1', :name => 'Jane Doe'
+
+user.add_role :admin
+user2.add_role :user
 
 unless Site.find_by_name("Drexel URBN Center")
-    site = Site.new :name => "Drexel URBN Center", :description => "This is a test site at the TechCamp hackathon", :address => "3501 Market Street", :zipcode => 19104
+  site = Site.new :name => "Drexel URBN Center", :description => "This is a test site at the TechCamp hackathon", :address => "3501 Market Street", :zipcode => 19104
 
-    site.save
+  site.save
 end
 
 site = Site.find_by_name("Drexel URBN Center")

fc5e31dee2990eadfad3b5fd40bd2a1db2931315
Kevin Clough <kevinclough@gmail.com>
2014-03-09 10:51:42 -0400

    fix query to work with PG

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -1,9 +1,25 @@
 class SummaryController < ApplicationController
   def index
-    @results = NodeReading
-        .group('node_id')
-        .order('collection_time DESC')
+    # todo refactor so this doesn't require two queries
 
+    conn = ActiveRecord::Base.connection
+    sql = 'SELECT ' +
+      'nr.id ' +
+      'FROM ' +
+      'node_readings nr ' +
+      'JOIN (' +
+      'SELECT max("node_readings"."id") as id, ' +
+      '"node_readings"."node_id" ' +
+      'FROM "node_readings"  ' +
+      'GROUP BY "node_readings"."node_id" ' +
+      ') sub ' +
+      'ON nr.id = sub.id'
+    
+    res = conn.execute sql
+
+    @results = NodeReading
+        .find_all_by_id(res.map { |x| x['id'] })
+    
     @weather = WeatherLocation.all.map do |w|
         #TODO Should probably cache this response: http://stackoverflow.com/questions/11308101/ruby-on-rails-how-to-cache-the-result-of-a-response-of-an-api-service
         ForecastIO.forecast(w.lat, w.lon)

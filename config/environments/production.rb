362a7c4688b8bcb5b442060f1b3a4e65f0aeb37e
Kevin Clough <kevinclough@gmail.com>
2014-03-09 10:02:17 -0400

    attempt to fix heroku cors issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,12 @@ SolarSunflower::Application.configure do
     password: ENV["GMAIL_PASSWORD"]
   }
 
-
+  config.middleware.use Rack::Cors do
+    allow do
+      origins '*'
+      resource '/public/*', :headers => :any, :methods => [:get, :post, :options]
+    end
+  end
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)

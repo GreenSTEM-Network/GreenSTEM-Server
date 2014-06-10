a88ea78f209222a3b3805033abe05831ab372beb
Kevin Clough <kevinclough@gmail.com>
2014-06-10 19:37:38 -0400

    change app name

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,7 +8,7 @@ RailsAdmin.config do |config|
   ################  Global configuration  ################
 
   # Set the admin name here (optional second array element will appear in red). For example:
-  config.main_app_name = ['Solar Sunflower', 'Admin']
+  config.main_app_name = ['Green Stem Network', 'Admin']
   # or for a more dynamic name:
   # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
 

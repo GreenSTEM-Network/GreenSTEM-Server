d943f8b2c6c00f390c50a9eb3043ee5b78063506
Kevin Clough <kevinclough@gmail.com>
2014-06-10 19:36:24 -0400

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SolarSunflower::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
-  authenticated :user do
-    root :to => 'home#index'
-  end
+  # authenticated :user do
+  #   root :to => 'home#index'
+  # end
   root :to => "home#index"
   devise_for :users
 

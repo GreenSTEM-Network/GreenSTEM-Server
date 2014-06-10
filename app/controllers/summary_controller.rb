99c40c9858f4ffd5d024281760f00dca101d6922
Kevin Clough <kevinclough@gmail.com>
2014-06-10 19:39:03 -0400

    clean up summary controller and fix sort by

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -3,35 +3,39 @@ class SummaryController < ApplicationController
     # todo refactor so this doesn't require two queries
 
     conn = ActiveRecord::Base.connection
-    sql = 'SELECT ' +
-      'nr.id ' +
+
+    sql = 
+      'SELECT ' +
+        'nr.id ' +
       'FROM ' +
-      'node_readings nr ' +
+        'node_readings nr ' +
       'JOIN (' +
-      'SELECT max("node_readings"."id") as id, ' +
-      '"node_readings"."node_id" ' +
-      'FROM "node_readings"  ' +
-      'GROUP BY "node_readings"."node_id" ' +
+        'SELECT max("node_readings"."id") as id, ' +
+          '"node_readings"."node_id" ' +
+          'FROM "node_readings"  ' +
+          'GROUP BY "node_readings"."node_id" ' +
       ') sub ' +
       'ON nr.id = sub.id'
     
     res = conn.execute sql
 
     @results = NodeReading
-        .find_all_by_id(res.map { |x| x['id'] })
+        .find(res.map { |x| x['id'] })
     
     @weather = WeatherLocation.all.map do |w|
         #TODO Should probably cache this response: http://stackoverflow.com/questions/11308101/ruby-on-rails-how-to-cache-the-result-of-a-response-of-an-api-service
         ForecastIO.forecast(w.lat, w.lon)
     end
 
-    json_results = {:readings => @results, :weather => @weather}
+    sorted_results =  @results.as_json.sort{ |x| x['site_id']}
+
+    json_results = {readings: sorted_results, weather: @weather}
 
     respond_to do |format|
       format.html { render json: json_results}
 
       if params[:callback]
-        format.js { render :json => json_results, :callback => params[:callback] }
+        format.js { render json: json_results, callback: params[:callback] }
       else
         format.json { render json: json_results}
       end

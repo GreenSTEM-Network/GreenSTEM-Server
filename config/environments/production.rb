1768d801a7c06bd79c96a91aa99d65edb16f8149
Kevin Clough <kevinclough@gmail.com>
2014-06-12 18:28:33 -0400

    increase logging to debug heroku issue again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,9 @@ SolarSunflower::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.consider_all_requests_local = true
+  config.action_view.debug_rjs                         = true
+  config.action_controller.perform_caching             = false
 
   config.serve_static_assets = true
 
